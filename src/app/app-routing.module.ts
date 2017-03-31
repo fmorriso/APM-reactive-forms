7a5e8e342fcf298029642f9491ffb538016818f0
Fred Morrison <fpmorrison@verizon.net>
2017-03-31 15:58:46 -0400

    1. added Welcome component 2. added Routes for routing 3. fixed img reference path error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+
+  { path: 'welcome', component: WelcomeComponent },
+
+  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
