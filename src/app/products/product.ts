b8fd885ec478d443b32d342e801b453014e44257
Fred Morrison <fpmorrison@verizon.net>
2017-04-11 21:41:17 -0400

    upgraded to angular 4.0.2 added products code

---
+++ src/app/products/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+  productId: number;
+  productName: string;
+  productCode: string;
+  releaseDate: string;
+  price: number;
+  description: string;
+  starRating: number;
+  imageUrl: string;
+}
