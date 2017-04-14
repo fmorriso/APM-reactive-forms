fdef432d46f17466a0c454dffa7d061bcd1c7878
Fred Morrison <fpmorrison@verizon.net>
2017-04-14 14:30:23 -0400

    Moved PageNotFoundComponent from SharedModule to AppModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { WelcomeComponent } from './welcome/welcome.component';
-// import {ProductListComponent} from './products/product-list.component';
-// import {ProductComponent} from './products/product.component';
-// import {ProductDetailComponent} from './products/product-detail.component';
-// import {ProductEditComponent} from './products/product-edit.component';
 import {PageNotFoundComponent} from './shared/page-not-found.component';
 
 const routes: Routes = [
@@ -19,11 +16,7 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 
-// { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
-//   ProductComponent,
-//   ProductDetailComponent,
-// ProductEditComponent,
-// PageNotFoundComponent
 export const routableComponents = [
+  PageNotFoundComponent,
   WelcomeComponent
 ];
